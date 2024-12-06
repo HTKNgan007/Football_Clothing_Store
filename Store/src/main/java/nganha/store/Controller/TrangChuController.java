9e292fb07d00f5ad1417e83a6bd9bcde2ff44d88
HTKNgan007 <nganha2004nh@gmail.com>
2024-12-06 11:32:28 +0700

    Chinh sua them giao dien

--- Store/src/main/java/nganha/store/Controller/TrangChuController.java
+++ Store/src/main/java/nganha/store/Controller/TrangChuController.java
@@ -1,15 +1,29 @@
 package nganha.store.Controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
+import java.util.Optional;
 
 public class TrangChuController {
+
   @FXML
   private StackPane contentPane;
+
+  @FXML
+  public void initialize() {
+    loadContent("QLDonHang.fxml");
+  }
 
   @FXML
   private void handleQLSanPham() {
@@ -26,8 +40,26 @@ public class TrangChuController {
     loadContent("QLDonHang.fxml");
   }
   @FXML
-  private void handleTrangChu() {
+  private void handleKhachHang() {
     loadContent("TrangChu.fxml");
+  }
+
+  @FXML
+  private void handleDangXuat(ActionEvent event) throws IOException {
+    // Hiển thị hộp thoại xác nhận
+    Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+    alert.setTitle("Xác nhận thoát");
+    alert.setHeaderText("Bạn có chắc chắn muốn đăng xuất?");
+
+    // Xử lý lựa chọn
+    Optional<ButtonType> result = alert.showAndWait();
+    if (result.isPresent() && result.get() == ButtonType.OK) {
+      try {
+        loadFXML("/nganha/store/login.fxml", event);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
   }
 
   private void loadContent(String fxmlFile) {
@@ -42,5 +74,20 @@ public class TrangChuController {
       System.err.println("Could not load FXML file: " + fxmlFile);
       e.printStackTrace();
     }
+  }
+
+  private void loadFXML(String fxmlPath, javafx.event.ActionEvent event) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(fxmlPath));
+    Parent root = fxmlLoader.load();
+
+    // Lấy Stage hiện tại từ sự kiện
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+    // Chuyển sang Scene mới
+    stage.setScene(new Scene(root));
+//    stage.initStyle(StageStyle.DECORATED);
+    stage.setResizable(true);
+    stage.centerOnScreen();
+    stage.show();
   }
 }
